95cfba16aab3febb4cec53926f587f10ff401989
jaymarspin <jaydaligdig123@gmail.com>
2019-04-17 16:59:18 +0800

    validation

--- src/app/moderate/title-edit/title-edit.page.ts
+++ src/app/moderate/title-edit/title-edit.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
-
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-title-edit',
   templateUrl: './title-edit.page.html',
@@ -7,7 +7,20 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class TitleEditPage implements OnInit {
   @Input('title') title
-  constructor() { }
+  former: FormGroup
+  constructor(private validators: Validators,private formBuilder: FormBuilder) { 
+    this.former = this.formBuilder.group({
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(50),
+        
+      ]))
+      
+     
+    });
+
+  }
   
   ngOnInit() {
   }
